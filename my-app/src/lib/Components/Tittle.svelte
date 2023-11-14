613de617fbfa2b2d449c43441472d95b2fd52cb7
Hubert Suchorowski <hubert.suchorowski1@gmail.com>
2023-11-14 11:18:47 +0100

    First version Tittle

---
+++ my-app/src/lib/Components/Tittle.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Avatar from '$lib/Avatar.png';
+</script>
+
+<body>
+	<div class="main">
+		<div class="Container">
+			<img src={Avatar} alt="Gostek" />
+		</div>
+	</div>
+	<slot />
+</body>
+
+<style lang="scss">
+	.main {
+		height: 80vh;
+		width: 100%;
+		background-color: #1abc9c;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-wrap: wrap;
+		.Container {
+			height: 80vh;
+			width: 30vw;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+	}
+</style>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import Nav from '../lib/Components/nav.svelte';
+	import Main from '../lib/Components/Tittle.svelte';
 	import Styles from '../lib/style/app.scss';
 </script>
 
 <body>
 	<Nav />
+	<Main />
 </body>
 
 <style lang="scss">
