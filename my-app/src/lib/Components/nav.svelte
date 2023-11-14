d2da2c12ba9a5fdf07e6d169d6d70ce8818d6bb8
Hubert Suchorowski <hubert.suchorowski1@gmail.com>
2023-11-14 09:44:56 +0100

    Initial Svelte
    Initial Svelte and test does it work.

---
+++ my-app/src/lib/Components/nav.svelte
@@ -0,0 +1,7 @@
+<body>
+	<div class="Navbar">test</div>
+	<slot />
+</body>
+
+<style lang="scss">
+</style>
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Nav from '../lib/Components/nav.svelte';
+</script>
+
+<body>
+	<Nav />
+</body>
+
+<style lang="scss">
+</style>
